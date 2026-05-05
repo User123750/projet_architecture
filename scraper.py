d04d31915a70879c7edb53ced884b6ae1ea0cf49
reyane59 <rchamseddine44@gmail.com>
2026-05-05 17:25:25 +0100

    tache 2

--- scraper.py
+++ scraper.py
@@ -3,7 +3,8 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import time
 import json  
-
+import boto3 
+from botocore.exceptions import ClientError 
 
 def scraper_article_hespress(url):
     headers = {'User-Agent': 'Mozilla/5.0'}
@@ -58,3 +59,28 @@ if __name__ == "__main__":
         
     pd.DataFrame(articles_hespress).to_csv("hespress_brut.csv", index=False, encoding='utf-8-sig')
     print(" Fichier hespress_brut.csv est créé avec succès !")
+    print("\n ENVOI VERS LE DATA LAKE (MINIO)...")
+    
+    
+    MINIO_ENDPOINT = 'http://localhost:9000' 
+    MINIO_ACCESS_KEY = 'admin_projet'
+    MINIO_SECRET_KEY = 'password_secure123'
+    BUCKET_NAME = 'bronze'
+
+    s3_client = boto3.client('s3',
+                             endpoint_url=MINIO_ENDPOINT,
+                             aws_access_key_id=MINIO_ACCESS_KEY,
+                             aws_secret_access_key=MINIO_SECRET_KEY)
+
+    try:
+        try:
+            s3_client.head_bucket(Bucket=BUCKET_NAME)
+        except ClientError:
+            s3_client.create_bucket(Bucket=BUCKET_NAME)
+
+        nom_fichier_destination = f"hespress_{int(time.time())}.csv" # Ajoute un timestamp pour ne pas écraser les anciens
+        s3_client.upload_file("hespress_brut.csv", BUCKET_NAME, nom_fichier_destination)
+        print(f"  Fichier envoyé avec succès dans le bucket '{BUCKET_NAME}' sous le nom '{nom_fichier_destination}' !")
+        
+    except Exception as e:
+        print(f"  Erreur lors de l'envoi vers MinIO : {e}")
